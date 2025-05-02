4e6d6050644b06dc4741a5de1ed20ca2539ff9f2
ZeroIRC <save3172@gmail.com>
2025-05-02 20:56:58 +0900

    feat: 로컬 스토리지 기능 추가

--- store/useTodoStore.ts
+++ store/useTodoStore.ts
@@ -1,4 +1,6 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
 
 export interface Todo {
   id: string;
@@ -13,27 +15,35 @@ interface TodoStore {
   deleteTodo: (id: string) => void;
 }
 
-export const useTodoStore = create<TodoStore>((set) => ({
-  todos: [],
-  addTodo: (text: string) =>
-    set((state) => ({
-      todos: [
-        ...state.todos,
-        {
-          id: Date.now().toString(),
-          text,
-          completed: false,
-        },
-      ],
-    })),
-  toggleTodo: (id: string) =>
-    set((state) => ({
-      todos: state.todos.map((todo) =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      ),
-    })),
-  deleteTodo: (id: string) =>
-    set((state) => ({
-      todos: state.todos.filter((todo) => todo.id !== id),
-    })),
-})); 
+export const useTodoStore = create<TodoStore>()(
+  persist(
+    (set) => ({
+      todos: [],
+      addTodo: (text: string) =>
+        set((state) => ({
+          todos: [
+            ...state.todos,
+            {
+              id: Date.now().toString(),
+              text,
+              completed: false,
+            },
+          ],
+        })),
+      toggleTodo: (id: string) =>
+        set((state) => ({
+          todos: state.todos.map((todo) =>
+            todo.id === id ? { ...todo, completed: !todo.completed } : todo
+          ),
+        })),
+      deleteTodo: (id: string) =>
+        set((state) => ({
+          todos: state.todos.filter((todo) => todo.id !== id),
+        })),
+    }),
+    {
+      name: 'todo-storage',
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+); 
